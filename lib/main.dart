b66710b90c8f084d3f432ab5cbf364779455d434
leoyun97 <leoyun@naver.com>
2023-02-07 17:46:01 +0900

    Code copy completed!!

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,10 @@ void main() {
   runApp(const MyApp());
 }
 
+void _onClicked() {
+  print('Clicked!');
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -19,84 +23,134 @@ class MyApp extends StatelessWidget {
         //   child: Text('Hello Flutter'),
         // ),
         backgroundColor: Color(0xff181818),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20,
-          ),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 80,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-                    const Text(
-                      'Hey, Leoyun',
-                      style: TextStyle(
-                        fontFamily: '굴림',
-                        color: Colors.white,
-                        fontSize: 28,
-                        fontWeight: FontWeight.w800,
-                      ),
-                    ),
-                    Text(
-                      'Welcome back',
-                      style: TextStyle(
-                        color: Colors.grey.shade500,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ]),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(
-                        height: 100,
-                      ),
-                      Text(
-                        'Total Balance',
-                        style: TextStyle(
-                            color: Colors.white.withOpacity(0.6), fontSize: 25),
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+            ),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 80,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
                       const Text(
-                        '\$5 194 382',
+                        'Hey, Leoyun',
                         style: TextStyle(
+                          fontFamily: '굴림',
                           color: Colors.white,
-                          fontSize: 44,
-                          fontWeight: FontWeight.bold,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
                         ),
                       ),
-                    ],
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Colors.grey.shade500,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ]),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(
+                          height: 100,
+                        ),
+                        Text(
+                          'Total Balance',
+                          style: TextStyle(
+                              color: Colors.white.withOpacity(0.6), fontSize: 25),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        const Text(
+                          '\$5 194 382',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 44,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    SizedBox(
+                      height: 100,
+                    ),
+                    roundbutton(
+                        text: 'Transfer',
+                        bgColor: Colors.amber,
+                        txtColor: Colors.black),
+                    roundbutton(
+                        text: 'Request',
+                        bgColor: Color(0xff1f2123),
+                        txtColor: Colors.white),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text('Wallets',
+                        style: TextStyle(
+                          fontSize: 35,
+                          color: Colors.white,
+                        )),
+                    TextButton(
+                      onPressed: _onClicked,
+                      child: Text(
+                        'View All',
+                        style: TextStyle(fontSize: 25, color: Colors.grey),
+                      ),
+                    ),
+                  ],
+                ),
+                const piledBox(
+                  currency: 'Euro',
+                  valueName: '6 428',
+                  unitName: 'EUR',
+                  moneyIcon: Icons.euro_rounded,
+                  isInverted: true,
+                ),
+                Transform.translate(
+                  offset: const Offset(0, -50),
+                  child: const piledBox(
+                    currency: 'BitCoin',
+                    valueName: '5 208',
+                    unitName: 'BTC',
+                    moneyIcon: Icons.currency_bitcoin,
+                    isInverted: false,
                   ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  SizedBox(
-                    height: 100,
+                ),
+                Transform.translate(
+                  offset: const Offset(0, -100),
+                  child: const piledBox(
+                    currency: 'Dollar',
+                    valueName: '1 528',
+                    unitName: 'USD',
+                    moneyIcon: Icons.attach_money_rounded,
+                    isInverted: true,
                   ),
-                  roundbutton(
-                      text: 'Transfer',
-                      bgColor: Colors.amber,
-                      txtColor: Colors.black),
-                  roundbutton(
-                      text: 'Request',
-                      bgColor: Color(0xff1f2123),
-                      txtColor: Colors.white),
-                ],
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/widgets/buttons.dart
+++ lib/widgets/buttons.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class roundbutton extends StatelessWidget {
@@ -31,6 +30,76 @@ class roundbutton extends StatelessWidget {
             color: txtColor,
             fontWeight: FontWeight.bold,
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class piledBox extends StatelessWidget {
+  final String currency, valueName, unitName;
+  final IconData moneyIcon;
+  final bool isInverted;
+  final conBgcolor = const Color(0xff1f2123);
+
+  const piledBox({
+    super.key,
+    required this.currency,
+    required this.valueName,
+    required this.unitName,
+    required this.moneyIcon,
+    required this.isInverted,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+          color: isInverted ? conBgcolor : Colors.white,
+          borderRadius: BorderRadius.circular(20)),
+      child: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  currency,
+                  style: TextStyle(fontSize: 30, color: isInverted ? Colors.white : conBgcolor),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      valueName,
+                      style: TextStyle(fontSize: 25, color: isInverted ? Colors.white : conBgcolor),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      unitName,
+                      style: const TextStyle(fontSize: 25, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Transform.scale(
+              scale: 5,
+              child: Transform.translate(
+                offset: const Offset(0, 5),
+                child: Icon(
+                  moneyIcon,
+                  color: isInverted ? Colors.white : conBgcolor,
+                  size: 50,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
